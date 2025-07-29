7e00d68391d0fc3afdae22f5272f4bd24f2c55f1
ThisProgrammer54 <philip.schnitzer01@gmail.com>
2025-07-29 09:59:46 +0200

    improved debugger

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,6 @@
 -- Add any additional keymaps here
 
 -- delete not used
-
 vim.keymap.del("n", "<leader>K")
 vim.keymap.del("n", "<leader>S")
 vim.keymap.del("n", "<leader>,")
@@ -11,3 +10,9 @@ vim.keymap.del("n", "<leader>-")
 
 -- Terminal
 vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+
+-- debugging
+vim.keymap.set("n", "<F5>", require("dap").continue)
+vim.keymap.set("n", "<F10>", require("dap").step_over)
+vim.keymap.set("n", "<F11>", require("dap").step_into)
+vim.keymap.set("n", "<F12>", require("dap").step_out)
